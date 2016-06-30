58fa7138d7d327f45476c92438f464ad0717f131
Francisco P <francisco@democracyos.io>
2016-06-30 19:39:29 -0300

    add noticias slider

--- ext/home/home.js
+++ ext/home/home.js
@@ -3,11 +3,26 @@ import bus from 'bus'
 import { domRender } from '../../lib/render/render'
 import * as layout from '../layout/layout'
 import template from './template.jade'
+import topicStore from '../../lib/topic-store/topic-store'
+import NoticiasView from './view.js'
 
 page('/', layout.load, (ctx, next) => {
   let view = domRender(template)
   layout.set(view)
   bus.emit('page:render')
+  topicStore.findAll({forum: '5773d0dcb6567110006020cc'})
+    .then(topics => {
+      if (!topics) return console.log('no noticias')
+      new NoticiasView({ // eslint-disable-line no-new
+        container: document.querySelector('#noticias-slider'),
+        locals: {
+          topics: topics
+        }
+      })
+    })
+    .catch(err => {
+      console.error('Found Error %s', err)
+    })
 })
 
 page.exit('/', layout.unload)
---
+++ ext/home/view.js
@@ -0,0 +1,19 @@
+import view from '../../lib/view/mixin'
+import template from './noti-card/template.jade'
+import Flickity from 'flickity'
+
+export default class NoticiasView extends view('appendable', 'withEvents') {
+  constructor (options = {}) {
+    options.template = template
+    super(options)
+    this.switchOn()
+  }
+
+  switchOn () {
+    new Flickity('#noticias-slider', { // eslint-disable-line no-new
+      wrapAround: true,
+      contain: true,
+      pageDots: false
+    })
+  }
+}
