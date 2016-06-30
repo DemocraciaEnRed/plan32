48646c765bc4e13b3ed3a26a148666bc29340710
Francisco P <francisco@democracyos.io>
2016-06-30 20:39:59 -0300

    fix slider template

--- ext/home/view.js
+++ ext/home/view.js
@@ -1,19 +1,22 @@
 import view from '../../lib/view/mixin'
-import template from '../topic-cards/template.jade'
+import template from './noticias-cards.jade'
 import Flickity from 'flickity'
 
 export default class NoticiasView extends view('appendable', 'withEvents') {
   constructor (options = {}) {
     options.template = template
     super(options)
+    this.flickity = undefined
     this.switchOn()
   }
 
   switchOn () {
-    new Flickity('#noticias-slider', { // eslint-disable-line no-new
-      wrapAround: true,
-      contain: true,
-      pageDots: false
-    })
+    if (this.flickity) return
+    this.flickity = new Flickity('#noticias-slider', // eslint-disable-line no-new
+      {
+        wrapAround: true,
+        contain: true,
+        pageDots: false
+      })
   }
 }
