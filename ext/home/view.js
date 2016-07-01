537f9c16a4fc12489263ed5f197f9e2414817505
Francisco P <francisco@democracyos.io>
2016-06-30 21:04:59 -0300

    update noticias slider

--- ext/home/view.js
+++ ext/home/view.js
@@ -1,9 +1,5 @@
 import view from '../../lib/view/mixin'
-<<<<<<< HEAD
 import template from './noticias-cards.jade'
-=======
-import template from '../topics-cards/template.jade'
->>>>>>> d8a4e74ebbc58ca067ad326039d41c0ee9fb3076
 import Flickity from 'flickity'
 
 export default class NoticiasView extends view('appendable', 'withEvents') {
@@ -12,10 +8,12 @@ export default class NoticiasView extends view('appendable', 'withEvents') {
     super(options)
     this.flickity = undefined
     this.switchOn()
+    debugger
   }
 
   switchOn () {
     if (this.flickity) return
+    debugger
     this.flickity = new Flickity('#noticias-slider', // eslint-disable-line no-new
       {
         wrapAround: true,
