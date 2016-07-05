fb7025727b6dec4c05dc535eecbe67ce85606ca4
Francisco P <francisco@democracyos.io>
2016-07-05 14:04:15 -0300

    update home noticias cards Closes #27

--- ext/home/view.js
+++ ext/home/view.js
@@ -1,10 +1,12 @@
 import view from '../../lib/view/mixin'
 import template from './noticias-cards.jade'
 import Flickity from 'flickity'
+import moment from 'moment'
 
 export default class NoticiasView extends view('appendable', 'withEvents') {
   constructor (options = {}) {
     options.template = template
+    options.locals.moment = moment
     super(options)
     this.flickity = undefined
     this.switchOn()
