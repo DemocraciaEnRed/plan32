5a98d0a5b5b3b7705a23fac5b45f0d0c62225081
Matías Lescano <matiasj.lescano@gmail.com>
2016-07-15 19:20:50 -0300

    add load home cover video progressively Closes #33

---
+++ ext/youtube-video/youtube-video.js
@@ -0,0 +1,86 @@
+import load from '@segment/load-script'
+import fit from 'fit.js'
+import waitUntil from 'wait-until'
+
+export default function youtubeVideo (container) {
+  if (window.YT && window.YT.Player) {
+    init(container)
+  } else {
+    load('https://www.youtube.com/iframe_api', function (err) {
+      if (err) throw err
+      waitUntil()
+        .interval(300)
+        .times(3000)
+        .condition(() => window.YT && window.YT.Player)
+        .done(function () { init(container) })
+    })
+  }
+}
+
+function init (container) {
+  const id = container.dataset.youtubeVideo
+
+  if (!id) throw new Error('[data-youtube-video] param missing.')
+
+  container.classList.add('loading')
+
+  if (window.getComputedStyle(container).position === 'static') {
+    container.style.position = 'relative'
+  }
+
+  const video = document.createElement('div')
+  const wrapper = document.createElement('div')
+  const iframe = document.createElement('div')
+
+  video.classList.add('video')
+  wrapper.classList.add('video-wrapper')
+
+  video.appendChild(wrapper)
+  wrapper.appendChild(iframe)
+
+  container.insertBefore(video, container.firstChild)
+
+  window.requestAnimationFrame(function () {
+    fit(wrapper, video, {cover: true, watch: true})
+  })
+
+  const player = new window.YT.Player(iframe, {
+    videoId: id,
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+      loop: 1,
+      playsinline: 0,
+      rel: 0,
+      showinfo: 0,
+      theme: 'dark',
+      suggestedQuality: 'highres',
+      playlist: [id]
+    },
+    events: {
+      onReady: function () { player.mute() },
+      onStateChange: onPlayerStateChange
+    }
+  })
+
+  let loopInterval
+  function onStart () {
+    loopInterval = setInterval(function () {
+      player.seekTo(0)
+    }, Math.round(player.getDuration() * 1000) - 500)
+    container.classList.remove('loading')
+  }
+
+  function onStop () {
+    clearInterval(loopInterval)
+    container.classList.add('loading')
+  }
+
+  function onPlayerStateChange (evt) {
+    if (window.YT.PlayerState.PLAYING === player.getPlayerState()) {
+      onStart()
+    } else {
+      onStop()
+    }
+  }
+}
